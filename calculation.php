499d781aa8bcbdac2618fd8f978b5eb3456b65e0
qiushan <923559948@qq.com>
2019-12-09 10:44:45 +0800

    update

--- calculation.php
+++ calculation.php
@@ -3,19 +3,12 @@ require_once('./excelOneData.php');
 require_once('./excelTwoData.php');
 $N = $_POST['N'];//产品数量
 $M = $_POST['M'];//每组数量
-$onePercentage = $_POST['onePercentage'];
-$twoPercentage = $_POST['twoPercentage'];
-$N1 = round($N/$M);//组数
+$onePercentage = $_POST['onePercentage']?$_POST['onePercentage']:0.95;
+$twoPercentage = $_POST['twoPercentage']?$_POST['twoPercentage']:0.85;
+$N1 = ceil($N/$M);//组数
 $groupNum = $N1;
 $x1 = floor($N1*(1-$onePercentage));
 $x2 = floor($N1*(1-$twoPercentage));
-if($x1<=$x2){
-    $result_X = $x1;
-    $result = 1;
-}else{
-    $result_X = $x2;
-    $result = 2;
-}
 
 $nArray= array_keys($oneData);
 if(!in_array($N1,$nArray)){
@@ -24,30 +17,28 @@ if(!in_array($N1,$nArray)){
     $key = array_search($N1,$nArray);
     $N1 = $nArray[$key+1];
 }
-$data =null;
+$data =[];
 $message ='success';
 $code =1;
-if($result_X < 2){
-    $result_X =2;
+if($x1 < 2){
+    $x1 =2;
 }
-if($result==1){
-    if(isset($oneData[$N1][$result_X])){
-        $data = $oneData[$N1][$result_X];
-    }else{
-        $message = '1方案不存在该n值';
-        $code =0;
-    }
-
+if($x2 < 2){
+    $x2 =2;
+}
+if(isset($oneData[$N1][$x1])){
+    $n1 = '方案一n为:'.$oneData[$N1][$x1];
 }else{
-    if(isset($twoData[$N1][$result_X])){
-        $data = $twoData[$N1][$result_X];
-    }else{
-        $message = '2方案不存在该n值';
-        $code =0;
-    }
+    $n1 = '1方案不存在该n值';
 }
 
-$zuobiao = '('.$N1.','.$result_X.')';
-$finalResult = array('zuobiao'=>$zuobiao,'groupnum'=>$groupNum,'fangan1'=>$x1,'fangan2'=>$x2,'data'=>$data,'result'=>$result);
+if(isset($twoData[$N1][$x2])){
+    $n2 = '方案二n为:'.$twoData[$N1][$x2];
+}else{
+    $n2 = '2方案不存在该n值';
+}
+$zuobiao1 = '方案一坐标为('.$N1.','.$x1.')';
+$zuobiao2 = '方案二坐标为('.$N1.','.$x2.')';
+$finalResult = array('zuobiao1'=>$zuobiao1,'zuobiao2'=>$zuobiao2,'groupnum'=>$groupNum,'fangan1'=>$x1,'fangan2'=>$x2,'n1'=>$n1,'n2'=>$n2);
 $array = array('code'=>$code,'data'=>$finalResult,'msg'=>$message);
 echo json_encode($array);die();
